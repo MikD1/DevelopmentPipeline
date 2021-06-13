ccaa7fdb596e723c4b24538a6964350ab3acf502
Mikhail Doshevsky <m.doshevsky@gmail.com>
2021-06-14 01:09:54 +0300

    Update DemoApp

--- DemoApp/DemoApp.Tests/ContactDtoTest.cs
+++ DemoApp/DemoApp.Tests/ContactDtoTest.cs
@@ -8,7 +8,8 @@ namespace DemoApp.Tests
     {
         [DataTestMethod]
         [DataRow("Aa", "Bb", "Aa Bb")]
-        [DataRow("Hello", "World", "Hello World")]
+        [DataRow("Hello", "World2", "Hello World")]
+        [DataRow("123", "456", "123 456")]
         public void GetFullNameIsCorrect(string firstName, string secondName, string expected)
         {
             ContactDto contact = new() {FirstName = firstName, SecondName = secondName};
